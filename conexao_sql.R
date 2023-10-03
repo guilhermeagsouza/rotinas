8f64ac6aa5a9fea784e6dcf3aa5a5e0874b19425
Guilherme Souza <guilherme.agsouza@hotmail.com>
2023-10-03 10:44:15 -0300

    Create conexao_sql.R

---
+++ conexao_sql.R
@@ -0,0 +1,2 @@
+# Conexão com o SQL; Não precisa de control+shift+enter para truncar o código. Os comandos abaixo permitem a conexão sem truncar!
+source('scripts_dashboard_book_crm/0.conecta_banco.R', echo=TRUE, max.deparse.length=60)
