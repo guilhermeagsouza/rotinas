3786c4aca9d3f0ad18e8063b8ba303781aed5128
Guilherme Souza <guilherme.agsouza@hotmail.com>
2023-10-03 10:45:25 -0300

    Update conexao_sql.R

--- conexao_sql.R
+++ conexao_sql.R
@@ -1,2 +1,12 @@
+#### Conexão com o banco ####
+#con <- dbConnect(
+#  odbc::odbc(), 
+#  Driver = "SQL Server", 
+#  Server = "vmintegracao", # remember \\ if your path has a \ 
+#  Database = "VM_INTEGRACAO",
+#  user = "MUNDIAL\\guilherme.souza", # remember \\ if your username has a \
+#  Trusted_Connection = "True"
+#)
+
 # Conexão com o SQL; Não precisa de control+shift+enter para truncar o código. Os comandos abaixo permitem a conexão sem truncar!
 source('scripts_dashboard_book_crm/0.conecta_banco.R', echo=TRUE, max.deparse.length=60)
